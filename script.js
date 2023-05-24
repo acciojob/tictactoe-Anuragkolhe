261690d4c2f01a7bda7bef81923751c1bf7fa166
Anuragkolhe <128228032+Anuragkolhe@users.noreply.github.com>
2023-05-24 13:54:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let player1 = '';
+let player2 = '';
+let currentPlayer = '';
+let board = ['', '', '', '', '', '', '', '', ''];
+const winningCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 
